dd21ae7329236b77b7b528d288afe92fc665eb98
mananiko005 <166157246+mananiko005@users.noreply.github.com>
2024-10-08 21:25:17 +0400

    change

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ let inputEl = document.getElementById(`input-list`);
 let btnEl = document.getElementById(`btn-list`);
 let ulEl = document.getElementById(`ul-list`);
 let clearall = document.getElementById("clearall");
+let formEl=document.getElementById("form-element")
 
-btnEl.addEventListener(`click`, function () {
+formEl.addEventListener(`submit`, function (event) {
+  event.preventDefault()
   let inputValue = inputEl.value;
   if (inputValue === ``) {
     return;
   }
 
   let li = document.createElement(`li`);
+  li.classList=`liitem`
 
   let btndelete = document.createElement("button");
   btndelete.innerHTML = '<i class="fas fa-trash"></i>';
+  btndelete.classList="btnitem"
   btndelete.addEventListener("click", function () {
     li.remove();
   });
